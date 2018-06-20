d2b6e6a93606ca423b3b5fe297eb23e1d27116c3
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2018-06-20 15:03:53 -0300

    Melhorias no robo
    Melhorias no robo

--- Robot_Larissa_Anderson/Larirobo.cs
+++ Robot_Larissa_Anderson/Larirobo.cs
@@ -7,16 +7,57 @@ using System.Threading.Tasks;
 
 namespace Robot_Larissa_Anderson
 {
-    public class Larirobo : Robot
+    public class Larirobo : AdvancedRobot
     {
+        Boolean Angulo = true;
         public override void Run()
-        {
+        {            
             while(true)
             {
-                
-                Ahead(200);
-                TurnLeft(90);
-                Fire(1);
+                if (Angulo == true)
+                {
+
+
+                    for (int voltasDir = 0; voltasDir < 5; voltasDir++)
+                    {
+                        Ahead(100);
+                        TurnLeft(90);
+                        TurnGunRight(90);
+                        Fire(1);
+
+                    }
+                    for (int voltasEsq = 5; voltasEsq > 0; voltasEsq++)
+                    {
+                        Ahead(200);
+                        TurnLeft(90);
+                        TurnGunRight(180);
+                        Fire(1);
+
+                    }
+                    Angulo = false;
+
+                }
+                else
+                {
+                    for (int voltasDir45 = 0; voltasDir45 < 8; voltasDir45++)
+                    {
+                        Ahead(50);
+                        TurnLeft(45);
+                        TurnGunRight(90);
+                        Fire(1);
+
+                    }
+                    for (int voltasEsq = 8; voltasEsq > 0; voltasEsq++)
+                    {
+                        Ahead(50);
+                        TurnLeft(45);
+                        TurnGunRight(180);
+                        Fire(1);
+
+                    }
+                    Angulo = true;
+                }
+
             }
         }
     }
