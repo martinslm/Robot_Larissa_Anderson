18bce881ed5af5dbe472bcb821afdf8b537233e9
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2018-06-19 21:53:53 -0300

    Adicionado tiros

---
+++ Robot_Larissa_Anderson/Larirobo.cs
@@ -0,0 +1,23 @@
+﻿using Robocode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot_Larissa_Anderson
+{
+    public class Larirobo : Robot
+    {
+        public override void Run()
+        {
+            while(true)
+            {
+                
+                Ahead(200);
+                TurnLeft(90);
+                Fire(1);
+            }
+        }
+    }
+}
