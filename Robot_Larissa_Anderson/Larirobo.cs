e948caffdc54b2604e2f5f0ecbe3349131cefcb5
andersonwagner06 <36700852+andersonwagner06@users.noreply.github.com>
2018-06-21 21:14:40 -0300

    Melhorias

--- Robot_Larissa_Anderson/Larirobo.cs
+++ Robot_Larissa_Anderson/Larirobo.cs
@@ -16,13 +16,10 @@ namespace Robot_Larissa_Anderson
             while (true)
             {
                 SetAhead(100);
-                SetTurnRight(90);
+                SetTurnRight(80);
                 SetTurnGunRight(360);
                 Execute();
 
-                //TurnLeft(180);
-                //TurnLeft(180);
-                //TurnGunLeft(180);
             }
         }
         //public override void OnScannedRobot(ScannedRobotEvent evnt)
@@ -35,15 +32,14 @@ namespace Robot_Larissa_Anderson
             if (e.Distance < 50 && Energy > 50)
             {
                 Fire(5);
-            } // caso contrário, atira com intensidade 2.
+            } // caso contrário, atira com intensidade 3.
             else
             {
                 Fire(3);
             }
             TurnRight(e.Bearing);
             Ahead(e.Distance + 5);
-            // Depois de atirar chama o radar novamente,
-            // antes de girar o canhão
+            // Depois de atirar chama o radar novamente, antes de girar o canhão
             Scan();
         }        public override void OnHitRobot(HitRobotEvent e)
         {
@@ -51,25 +47,25 @@ namespace Robot_Larissa_Anderson
             // Faz o cálculo da intensidade necessária para enfraquecer o oponente
             if (e.Energy > 16)
             {
-                Fire(3);
+                Fire(4);
             }
             else if (e.Energy > 10)
             {
-                Fire(2);
+                Fire(3);
             }
             else if (e.Energy > 4)
             {
-                Fire(1);
+                Fire(2);
             }
             else if (e.Energy > 2)
             {
-                Fire(.5);
+                Fire(1.5);
             }
             else if (e.Energy > .4)
             {
                 Fire(.1);
             }
-            Ahead(50); // E avança para se chocar com ele
+            Ahead(40); // E avança para se chocar com ele
         }       
     }
 }
